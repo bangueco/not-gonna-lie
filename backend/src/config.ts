2a4bc1ec8f6d2bae895253756a6ec4f9cc7d0a66
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-17 21:52:24 +0800

    feat: add jwt secrets to environment

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-  PORT: process.env.PORT || 3000
+  PORT: process.env.PORT || 3000,
+  JWT_ACCESS_TOKEN_KEY: process.env.JWT_ACCESS_TOKEN_KEY || 'generic',
+  JWT_REFRESH_TOKEN_KEY: process.env.JWT_REFRESH_TOKEN_KEY || 'generic'
 }
 
 export default config
