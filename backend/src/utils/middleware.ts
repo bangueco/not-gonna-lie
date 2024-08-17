7c3054c8e2648e95ff4eb3b3d030dad4da0dcfc3
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-17 21:42:16 +0800

    feat: add middleware error handler

--- backend/src/utils/middleware.ts
+++ backend/src/utils/middleware.ts
@@ -1,9 +1,24 @@
 import { NextFunction, Request, Response } from "express";
+import { ApiError, ValidationError } from "./error";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 const unknownEndPoint = (_request: Request, response: Response, _next: NextFunction) => {
   return response.status(404).json({message: 'Unknown endpoint.'})
 }
 
+const errorHandler = (error: unknown, _request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof ApiError) {
+    return response.status(error.status).json({message: error.message})
+  } else if (error instanceof ValidationError) {
+    return response.status(error.status).json({field: error.field, message: error.message})
+  } else if (error instanceof PrismaClientKnownRequestError) {
+    return response.status(400).json({message: error.message})
+  }
+
+  next(error)
+}
+
 export default {
-  unknownEndPoint
+  unknownEndPoint,
+  errorHandler
 }
