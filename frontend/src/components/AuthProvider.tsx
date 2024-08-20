29c165737aa56ee1ad6a464328885ea8e69057e3
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-20 23:52:59 +0800

    feat: add auth provider

---
+++ frontend/src/components/AuthProvider.tsx
@@ -0,0 +1,29 @@
+import { UserToken } from "@/types/user";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+export const AuthContext = createContext<UserToken | null>(null)
+
+type AuthProps = {
+  children: ReactNode
+}
+
+export default function AuthProvider({children}: AuthProps) {
+  const [auth, setAuth] = useState<UserToken>()
+
+  useEffect(() => {
+    const user = localStorage.getItem('user')
+
+    if (!user) {
+      return
+    }
+    
+    const parsedUser: UserToken = JSON.parse(user)
+    setAuth(parsedUser)
+  }, [])
+
+  return (
+    auth 
+    ? <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+    : <></>
+  )
+}
