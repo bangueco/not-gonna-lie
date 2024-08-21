eb5efe07e833e13962a8be16def65c4c993e104e
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-21 15:47:53 +0800

    feat: add username parameter

--- backend/src/routers/user.route.ts
+++ backend/src/routers/user.route.ts
@@ -4,5 +4,6 @@ import userController from '../controllers/user.controller'
 const userRouter = express.Router()
 
 userRouter.get('/', userController.getUsers)
+userRouter.get('/:username', userController.getByUsername)
 
 export default userRouter
